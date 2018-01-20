e27b9bff19359e8e0089e0408d46d1afeaa47712
hard <harleydav@mail.ru>
2018-01-21 00:47:20 +0300

    activity parent-child

--- app/src/main/java/com/hard/Activity2.java
+++ app/src/main/java/com/hard/Activity2.java
@@ -2,27 +2,26 @@ package com.hard;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-public class Activity2 extends AppCompatActivity implements View.OnClickListener {
+public class Activity2 extends AppCompatActivity {
     private TextView textView;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity2);
 
         textView = findViewById(R.id.textView2);
 
-        textView.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        Intent intent = new Intent(this, Activity3.class);
-        startActivity(intent);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity2.this, Activity1.class);
+                startActivity(intent);
+            }
+        });
     }
 }
