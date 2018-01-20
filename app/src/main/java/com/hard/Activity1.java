e3a3779e18cd4c74088f104387ea3bf8fca62a3f
hard <harleydav@mail.ru>
2018-01-21 02:52:56 +0300

    context menu

--- app/src/main/java/com/hard/Activity1.java
+++ app/src/main/java/com/hard/Activity1.java
@@ -2,51 +2,59 @@ package com.hard;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.view.ContextMenu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Activity1 extends AppCompatActivity {
+    private TextView textView;
+
+    private static final class Id {
+        private static final int itemId1 = 1;
+        private static final int itemId2 = 2;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity1);
+
+        textView = findViewById(R.id.textView1);
+
+        registerForContextMenu(textView);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = super.getMenuInflater();
-        menuInflater.inflate(R.menu.menu_activity1, menu);
-
-        return true;
+    public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo menuInfo) {
+        switch (view.getId()) {
+            case R.id.textView1:
+                menu.add(0, Id.itemId1, 0, "Item1");
+                menu.add(0, Id.itemId2, 0, "Item2");
+                break;
+        }
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onContextItemSelected(MenuItem item) {
         int id = item.getItemId();
 
         switch (id) {
-            case R.id.action_item1:
+            case Id.itemId1:
                 String str1 = getString(R.string.action_item1);
 
                 Toast toast1 = Toast.makeText(Activity1.this, str1, Toast.LENGTH_SHORT);
                 toast1.show();
                 break;
-            case R.id.action_item2:
+            case Id.itemId2:
                 String str2 = getString(R.string.action_item2);
 
                 Toast toast2 = Toast.makeText(Activity1.this, str2, Toast.LENGTH_SHORT);
                 toast2.show();
                 break;
-            case R.id.action_item3:
-                String str3 = getString(R.string.action_item3);
-
-                Toast toast3 = Toast.makeText(Activity1.this, str3, Toast.LENGTH_SHORT);
-                toast3.show();
-                break;
         }
 
-        return super.onOptionsItemSelected(item);
+        return super.onContextItemSelected(item);
     }
 }
